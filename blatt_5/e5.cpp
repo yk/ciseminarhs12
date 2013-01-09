820b57502b84c5f8e57bf53bb4904dbb02cd14bd
yk <yk@ykn.(none)>
2013-01-09 15:45:21 +0100

    blatt 5 advanced done

--- blatt_5/e5.cpp
+++ blatt_5/e5.cpp
@@ -29,12 +29,12 @@ void siftandsurf() {
 	showandsave("surfedges", edges);
 }
 
-vector<KeyPoint> getHarrisPoints(Mat& img,int thresh=95) {
+vector<KeyPoint> getHarrisPoints(Mat& img, int thresh = 95) {
 	Mat dst, dst_norm, dst_norm_scaled;
 	dst = Mat::zeros(img.size(), CV_32FC1);
 //	int thresh = 95;
 
-	/// Detector parameters
+/// Detector parameters
 	int blockSize = 2;
 	int apertureSize = 3;
 	double k = 0.04;
@@ -51,7 +51,7 @@ vector<KeyPoint> getHarrisPoints(Mat& img,int thresh=95) {
 	for (int j = 0; j < dst_norm.rows; j++) {
 		for (int i = 0; i < dst_norm.cols; i++) {
 			if ((int) dst_norm.at<float>(j, i) > thresh) {
-				v.push_back(KeyPoint(i,j,1));
+				v.push_back(KeyPoint(i, j, 1));
 			}
 		}
 	}
@@ -93,9 +93,8 @@ void harris() {
 	showandsave("harris_bookt", src_gray);
 }
 
-void match() {
-	Mat img = imread("data/library.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	Mat book = imread("data/book.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+pair<pair<vector<KeyPoint>, vector<KeyPoint> >, vector<DMatch>> match(Mat& img,
+		Mat& book, string bookname) {
 
 //	int minHessian = 400;
 
@@ -126,7 +125,7 @@ void match() {
 	std::vector<DMatch> matches;
 	matcher.match(descriptors_1, descriptors_2, matches);
 
-	float thresh = 0.2;
+	float thresh = 0.15;
 
 	auto newend =
 			remove_if(matches.begin(), matches.end(),
@@ -137,7 +136,8 @@ void match() {
 	drawMatches(img, keypoints_1, book, keypoints_2, good_matches, img_matches);
 
 	//-- Show detected matches
-	showandsave("Matches_surf_02", img_matches);
+	showandsave("matches_" + bookname, img_matches);
+	return make_pair(make_pair(keypoints_1, keypoints_2), good_matches);
 }
 
 float distPointLine(float a, float b, float x, float y) {
@@ -169,10 +169,146 @@ void ransac() {
 	cout << "a = " << a << ", b = " << b << endl;
 }
 
+void ransacmatch() {
+	string booknames[] = {"book","bookP","bookR","bookT"};
+	Mat img = imread("data/library.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	for(string& bookname : booknames){
+	Mat book = imread("data/" + bookname + ".jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	auto kpms = match(img, book,bookname);
+	auto matches = kpms.second;
+	auto qdesc = kpms.first.first;
+	auto tdesc = kpms.first.second;
+	auto tries = 10000;
+	Mat bestMat;
+	double bestScore = numeric_limits<double>::max();
+	for (int i = 0; i < tries; i++) {
+		vector<DMatch> selected;
+		Point2f src[3];
+		Point2f dst[3];
+		for (int j = 0; j < 3; j++) {
+			auto s = matches[rand() % matches.size()];
+//			if (find(selected.begin(), selected.end(), s) != selected.end()) {
+//				cout << "duplicate" << endl;
+//				j--;
+//				continue;
+//			}
+			selected.push_back(s);
+			src[j] = qdesc[s.queryIdx].pt;
+			dst[j] = tdesc[s.trainIdx].pt;
+		}
+		//calc affine transformation
+		Mat warp_mat = getAffineTransform(src, dst);
+		double error = 0;
+		for (auto it = matches.begin();it!=matches.end();it++) {
+			auto match = *it;
+			auto pointInOneImage = qdesc[match.queryIdx].pt;
+			auto pointInOtherImage = tdesc[match.trainIdx].pt;
+			cv::Mat point(3, 1, CV_64FC1, cv::Scalar(0));
+			point.at<double>(0) = pointInOneImage.x;
+			point.at<double>(1) = pointInOneImage.y;
+			point.at<double>(2) = 1.f;
+
+			cv::Mat calculatedPosition = warp_mat * point;
+
+			cv::Mat truePosition(2, 1, CV_64FC1);
+			truePosition.at<double>(0) = pointInOtherImage.x;
+			truePosition.at<double>(1) = pointInOtherImage.y;
+			error += cv::norm(truePosition, calculatedPosition);
+		}
+		if(error < bestScore){
+			bestScore = error;
+			bestMat = warp_mat;
+		}
+	}
+	cout << bookname << ": " << bestScore << endl;
+	Mat M;
+	invertAffineTransform(bestMat,M);
+	Mat wrpdst;
+	warpAffine(book,wrpdst,M, img.size());
+	Mat imcopy;
+	img.copyTo(imcopy);
+	imcopy -= wrpdst;
+	showandsave("warped " + bookname, imcopy);
+	}
+}
+
+vector<Mat> getGaussPyramid(Mat img, double minsigma, double maxsigma, int numsteps){
+	vector<Mat> pyr;
+	for(int step=0;step<numsteps;step++){
+		double sigma = minsigma + step*maxsigma/(numsteps-1);
+		Mat blrd;
+		GaussianBlur(img,blrd,Size2f(0,0),sigma);
+		pyr.push_back(blrd);
+	}
+	return pyr;
+}
+
+vector<Mat> getDiffPyramid(vector<Mat> pyramid){
+	vector<Mat> pyr;
+	for(int i=0;i<pyramid.size()-1;i++){
+		pyr.push_back(pyramid[i+1] - pyramid[i]);
+	}
+	return pyr;
+}
+
+#define easysift_global_maxima 1
+
+vector<Point> getExtrema(vector<Mat> diffPyr){
+	vector<Point> v;
+	for(auto& m : diffPyr){
+#if easysift_global_maxima
+		Point maxLoc;
+		minMaxLoc(abs(m),NULL,NULL,NULL,&maxLoc);
+		v.push_back(maxLoc);
+#else
+		Mat ma = abs(m);
+		for(int i=0;i<m.cols;i++){
+			for(int j=0;j<m.rows;j++){
+				bool max = true;
+				for(int x=-1;x<=1;x++){
+					for(int y=-1;y<=1;y++){
+						if(x != 0 && y != 0){
+							if(ma.at<float>(i+x,j+y) > ma.at<float>(i,j)){
+								max = false;
+							}
+						}
+					}
+				}
+				if(max){
+					Point2i p;
+					p.x = i;
+					p.y = j;
+					v.push_back(p);
+				}
+			}
+		}
+#endif
+	}
+	return v;
+}
+
+void easySIFT(){
+	Mat img = imread("data/library.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	auto gp = getGaussPyramid(img,0.01,10.0,100);
+	auto dp = getDiffPyramid(gp);
+	auto ex = getExtrema(dp);
+	Mat out;
+	vector<KeyPoint> keyPoints;
+	for(auto& p : ex){
+		KeyPoint kp;
+		kp.pt = p;
+		keyPoints.push_back(kp);
+	}
+	drawKeypoints(img,keyPoints,out);
+	showandsave("easysift",out);
+}
+
 int main() {
 //	siftandsurf();
 //	harris();
 //	match();
-	ransac();
+//	ransac();
+	ransacmatch();
+//	easySIFT();
 }
 
--- blatt_7/main.cpp
+++ blatt_7/main.cpp
@@ -1,173 +1,172 @@
+////
+////  main.cpp
+////  fundamentalMatrix
+////
+////  Created by Alex Attinger on 10.12.12.
+////  Copyright (c) 2012 Alex Attinger. All rights reserved.
+////
 //
-//  main.cpp
-//  fundamentalMatrix
+//#include <iostream>
+//#include "opencv2/opencv.hpp"
+//#include "opencv2/features2d/features2d.hpp"
+//#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/legacy/legacy.hpp"
 //
-//  Created by Alex Attinger on 10.12.12.
-//  Copyright (c) 2012 Alex Attinger. All rights reserved.
 //
-
-#include <iostream>
-#include "opencv2/opencv.hpp"
-#include "opencv2/features2d/features2d.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/nonfree/nonfree.hpp"
-#include "opencv2/legacy/legacy.hpp"
-
-
-using namespace cv;
-using namespace std;
-
-std::vector<Point2f> matchedPoints_1,matchedPoints_2;
-std::vector<KeyPoint> matchedKeyPoints_1,matchedKeyPoints_2;
-Mat view1,view2,outliers;
-
-
-void showandsave(string name, Mat img) {
-	//imwrite("data/res/" + name + ".png", img);
-	imshow(name, img);
-	waitKey();
-}
-
-Mat getHomogeneousMat(const Point2f &x){
-    double pointData[] = {x.x,x.y,1.0};
-    Mat point = Mat(3,1,CV_64FC1,pointData).clone();
-    return point;
-    
-}
-
-
-
-
-
-
-Mat calculateFundamental(){
-    Mat fundamentalRan;
-    
-       
-    fundamentalRan = findFundamentalMat(matchedPoints_1, matchedPoints_2,CV_FM_RANSAC,1.,.99,outliers);
-   
-    return fundamentalRan;
-}
-
-
-
-void match() {
-    
-//	view1 = imread("/Users/alexattinger_3/Dropbox/5_sem/ki_seminar/7/data/johnHunter/001.png", CV_LOAD_IMAGE_GRAYSCALE);
-//	view2 = imread("/Users/alexattinger_3/Dropbox/5_sem/ki_seminar/7/data/johnHunter/002.png", CV_LOAD_IMAGE_GRAYSCALE);
-//    
-//    
-//	view1 = imread("/Users/alexattinger_3/Dropbox/5_sem/ki_seminar/7/data/car/input1.png", CV_LOAD_IMAGE_GRAYSCALE);
-//	view2 = imread("/Users/alexattinger_3/Dropbox/5_sem/ki_seminar/7/data/car/input2.png", CV_LOAD_IMAGE_GRAYSCALE);
+//using namespace cv;
+//using namespace std;
 //
-            //
-//    
-   
-    
-	std::vector<KeyPoint> keypoints_1, keypoints_2;
-    SurfFeatureDetector detector(900);
-  
-    
-    
-	detector.detect(view1, keypoints_1);
-	detector.detect(view2, keypoints_2);
-    
-    
-    
-	//-- Step 2: Calculate descriptors (feature vectors)
-	SurfDescriptorExtractor extractor;
-    //	SiftDescriptorExtractor extractor;
-    
-	Mat descriptors_1, descriptors_2;
-    
-	extractor.compute(view1, keypoints_1, descriptors_1);
-	extractor.compute(view2, keypoints_2, descriptors_2);
-    
-    
-	//-- Step 3: Matching descriptor vectors with a brute force matcher
-	BruteForceMatcher<L2<float> > matcher;
-	std::vector<DMatch> matches;
-	matcher.match(descriptors_1, descriptors_2, matches);
-    
-	float thresh = 0.24;
-    
-	auto newend =
-    remove_if(matches.begin(), matches.end(),
-              [&](DMatch& match)->bool {/*cout<<match.distance<<endl;*/return (abs(match.distance) > thresh);});
-	auto good_matches = vector<DMatch>(matches.begin(), newend);
-	//-- Draw matches
-	Mat img_matches;
-	drawMatches(view1, keypoints_1, view2, keypoints_2, good_matches, img_matches);
-    cout<<keypoints_1[1].pt.x<<endl;
-    for(int i = 0;i<good_matches.size();i++){
-        matchedKeyPoints_1.push_back(keypoints_1[good_matches[i].queryIdx]);
-        matchedKeyPoints_2.push_back(keypoints_2[good_matches[i].trainIdx]);
-        matchedPoints_1.push_back(keypoints_1[good_matches[i].queryIdx].pt);
-        matchedPoints_2.push_back(keypoints_2[good_matches[i].trainIdx].pt);
-    }
-    
-	//-- Show detected matches
-	showandsave("Matches_surf_02", img_matches);
-}
-
-int minDisp = 64;
-int numDisp = 128;
-int SADSize = 3;
-int P1 = 5400;
-int P2 = 21600;
-string winname = "Disparity Map";
-Mat dest1,dest2;
-Mat dispMap;
-
-void processInput(int, void*){
-    int _minDisp = -minDisp;
-    int _numDisp = numDisp-numDisp%16;
-    int _SADsize = SADSize;
-    StereoSGBM bm(_minDisp, _numDisp, _SADsize,P1,P2);
-    bm(dest1,dest2,dispMap);
-    normalize(dispMap,dispMap, 0,256,CV_MINMAX,NULL);
-    //applyColorMap(dispMap, dispMap, cv::COLORMAP_JET);
-    //Mat disp_vis = Mat(dispMap.size(), CV_8U);
-    //convertScaleAbs(dispMap, disp_vis);
-    imshow(winname,dispMap);
-    
-}
-
-void setUpTrackbars(){
-    namedWindow(winname,1);
-    createTrackbar("minimum", winname, &minDisp, 128,processInput);
-    createTrackbar("num Disp",winname,&numDisp,500,processInput);
-    createTrackbar("Patch size",winname,&SADSize,21,processInput);
-    createTrackbar("P1",winname,&P1,7000,processInput);
-    createTrackbar("p2", winname, &P2, 30000,processInput);
-}
-
-
-
-int main(int argc, const char * argv[])
-{
-    //EX 1
-    match();
-    for(int i = 0; i<matchedPoints_1.size();i++){
-        cout<<matchedPoints_1[i].y-matchedPoints_2[i].y<<endl;
-    }
-    Mat f = calculateFundamental();
-    Mat h1,h2;
-
-    
-    //rectify images
-    stereoRectifyUncalibrated(matchedPoints_1, matchedPoints_2, f, view1.size(), h1, h2);
-    //apply the transformation
-    warpPerspective(view1, dest1, h1, view1.size());
-    warpPerspective(view2, dest2, h2, view1.size());
-    //show images
-    imshow("Rectified 1",dest1);
-    imshow("Rectified 2",dest2);
-    //do the matching
-    setUpTrackbars();
-    processInput(0, 0);
-
-    
-    waitKey();
-    return 0;
-}
+//std::vector<Point2f> matchedPoints_1,matchedPoints_2;
+//std::vector<KeyPoint> matchedKeyPoints_1,matchedKeyPoints_2;
+//Mat view1,view2,outliers;
+//
+//
+////void showandsave(string name, Mat img) {
+////	//imwrite("data/res/" + name + ".png", img);
+////	imshow(name, img);
+////	waitKey();
+////}
+//
+//Mat getHomogeneousMat(const Point2f &x){
+//    double pointData[] = {x.x,x.y,1.0};
+//    Mat point = Mat(3,1,CV_64FC1,pointData).clone();
+//    return point;
+//
+//}
+//
+//
+//
+//
+//
+//
+//Mat calculateFundamental(){
+//    Mat fundamentalRan;
+//
+//
+//    fundamentalRan = findFundamentalMat(matchedPoints_1, matchedPoints_2,CV_FM_RANSAC,1.,.99,outliers);
+//
+//    return fundamentalRan;
+//}
+//
+//
+//
+//void match() {
+//
+////	view1 = imread("/Users/alexattinger_3/Dropbox/5_sem/ki_seminar/7/data/johnHunter/001.png", CV_LOAD_IMAGE_GRAYSCALE);
+////	view2 = imread("/Users/alexattinger_3/Dropbox/5_sem/ki_seminar/7/data/johnHunter/002.png", CV_LOAD_IMAGE_GRAYSCALE);
+////
+////
+////	view1 = imread("/Users/alexattinger_3/Dropbox/5_sem/ki_seminar/7/data/car/input1.png", CV_LOAD_IMAGE_GRAYSCALE);
+////	view2 = imread("/Users/alexattinger_3/Dropbox/5_sem/ki_seminar/7/data/car/input2.png", CV_LOAD_IMAGE_GRAYSCALE);
+////
+//            //
+////
+//
+//
+//	std::vector<KeyPoint> keypoints_1, keypoints_2;
+//    SurfFeatureDetector detector(900);
+//
+//
+//
+//	detector.detect(view1, keypoints_1);
+//	detector.detect(view2, keypoints_2);
+//
+//
+//
+//	//-- Step 2: Calculate descriptors (feature vectors)
+//	SurfDescriptorExtractor extractor;
+//    //	SiftDescriptorExtractor extractor;
+//
+//	Mat descriptors_1, descriptors_2;
+//
+//	extractor.compute(view1, keypoints_1, descriptors_1);
+//	extractor.compute(view2, keypoints_2, descriptors_2);
+//
+//
+//	//-- Step 3: Matching descriptor vectors with a brute force matcher
+//	BruteForceMatcher<L2<float> > matcher;
+//	std::vector<DMatch> matches;
+//	matcher.match(descriptors_1, descriptors_2, matches);
+//
+//	float thresh = 0.24;
+//
+//	auto newend =
+//    remove_if(matches.begin(), matches.end(),
+//              [&](DMatch& match)->bool {/*cout<<match.distance<<endl;*/return (abs(match.distance) > thresh);});
+//	auto good_matches = vector<DMatch>(matches.begin(), newend);
+//	//-- Draw matches
+//	Mat img_matches;
+//	drawMatches(view1, keypoints_1, view2, keypoints_2, good_matches, img_matches);
+//    cout<<keypoints_1[1].pt.x<<endl;
+//    for(int i = 0;i<good_matches.size();i++){
+//        matchedKeyPoints_1.push_back(keypoints_1[good_matches[i].queryIdx]);
+//        matchedKeyPoints_2.push_back(keypoints_2[good_matches[i].trainIdx]);
+//        matchedPoints_1.push_back(keypoints_1[good_matches[i].queryIdx].pt);
+//        matchedPoints_2.push_back(keypoints_2[good_matches[i].trainIdx].pt);
+//    }
+//
+//	//-- Show detected matches
+//	showandsave("Matches_surf_02", img_matches);
+//}
+//
+//int minDisp = 64;
+//int numDisp = 128;
+//int SADSize = 3;
+//int P1 = 5400;
+//int P2 = 21600;
+//string winname = "Disparity Map";
+//Mat dest1,dest2;
+//Mat dispMap;
+//
+//void processInput(int, void*){
+//    int _minDisp = -minDisp;
+//    int _numDisp = numDisp-numDisp%16;
+//    int _SADsize = SADSize;
+//    StereoSGBM bm(_minDisp, _numDisp, _SADsize,P1,P2);
+//    bm(dest1,dest2,dispMap);
+//    normalize(dispMap,dispMap, 0,256,CV_MINMAX,NULL);
+//    //applyColorMap(dispMap, dispMap, cv::COLORMAP_JET);
+//    //Mat disp_vis = Mat(dispMap.size(), CV_8U);
+//    //convertScaleAbs(dispMap, disp_vis);
+//    imshow(winname,dispMap);
+//
+//}
+//
+//void setUpTrackbars(){
+//    namedWindow(winname,1);
+//    createTrackbar("minimum", winname, &minDisp, 128,processInput);
+//    createTrackbar("num Disp",winname,&numDisp,500,processInput);
+//    createTrackbar("Patch size",winname,&SADSize,21,processInput);
+//    createTrackbar("P1",winname,&P1,7000,processInput);
+//    createTrackbar("p2", winname, &P2, 30000,processInput);
+//}
+//
+//
+//
+//int main2(int argc, const char * argv[])
+//{
+//    //EX 1
+//    match();
+//    for(int i = 0; i<matchedPoints_1.size();i++){
+//        cout<<matchedPoints_1[i].y-matchedPoints_2[i].y<<endl;
+//    }
+//    Mat f = calculateFundamental();
+//    Mat h1,h2;
+//
+//
+//    //rectify images
+//    stereoRectifyUncalibrated(matchedPoints_1, matchedPoints_2, f, view1.size(), h1, h2);
+//    //apply the transformation
+//    warpPerspective(view1, dest1, h1, view1.size());
+//    warpPerspective(view2, dest2, h2, view1.size());
+//    //show images
+//    imshow("Rectified 1",dest1);
+//    imshow("Rectified 2",dest2);
+//    //do the matching
+//    setUpTrackbars();
+//    processInput(0, 0);
+//
+//
+//    waitKey();
+//    return 0;
+//}
